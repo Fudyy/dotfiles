ad3f5619dd518fa3b2064733a2e6f2f95ac1fb05
Carlos Gonzalez <carlosgonsalazar@gmail.com>
2025-05-17 18:45:56 -0400

    fix nvim config

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    opt = {
+      transparent_background = true,
+    },
+  },
+}
